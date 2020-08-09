00d0d88e9b0f40574bd561d72ba8b8a777aa7c0f
Fedot <fedotevseevmac@gmail.com>
2020-08-10 00:52:12 +0900

    import Realm

---
+++ DemoProject/Supporting files/StorageManager.swift
@@ -0,0 +1,13 @@
+//
+//  StorageManager.swift
+//  DemoProject
+//
+//  Created by Федот Евсеев on 10.08.2020.
+//  Copyright © 2020 Федот Евсеев. All rights reserved.
+//
+
+import RealmSwift
+
+class StorageManager {
+    let realm = try! Realm()
+}
