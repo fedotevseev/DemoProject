e31e9670ffbd884e09e4bf3b4c26cfc1dac9a13f
Fedot <fedotevseevmac@gmail.com>
2020-08-10 00:41:41 +0900

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Федот Евсеев on 10.08.2020.
+//  Copyright © 2020 Федот Евсеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
